4ec2693813a693db141e80d7e74d608587787a31
RoundLuka <lukagurgenidze282007@gmail.com>
2024-02-22 03:53:17 -0800

    fourth commit

--- Day 03/Homework/Family.py
+++ Day 03/Homework/Family.py
@@ -14,4 +14,9 @@ grandfather_surname = "Ambokadze"
 sister_name = "Anna"
 sister_surname = "Gurgenidze"
 
-print("my mother's name is",mom_name,"my mother's surname is",mom_surname,"my dad's name is",dad_name,"my dad's surname is",dad_surname,"my grandmother's name is",grandmother_name,"my grandmother's suranme is",grandmother_surname,"my grandfather's name is",grandfather_name,"my grandfather's surname is",grandfather_surname,"my sister's name is",sister_name,"my sister's surname is",sister_surname,"this is my family")
+print("my mother's name is",mom_name,"my mother's surname is",mom_surname,
+      "my dad's name is",dad_name,"my dad's surname is",dad_surname,
+      "my grandmother's name is",grandmother_name,"my grandmother's suranme is",
+      grandmother_surname,"my grandfather's name is",grandfather_name,
+      "my grandfather's surname is",grandfather_surname,"my sister's name is",
+      sister_name,"my sister's surname is",sister_surname,"this is my family")
