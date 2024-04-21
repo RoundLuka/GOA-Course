4fc2cf6098e4204545476174782ecf07580d33b9
RoundLuka <lukagurgenidze282007@gmail.com>
2024-04-21 10:22:29 -0700

    commit

---
+++ Day 33/Homework/codewars.py
@@ -0,0 +1,6 @@
+#I completed every codewars except those 2:
+#https://www.codewars.com/kata/587731fda577b3d1b0001196
+#https://www.codewars.com/kata/57f8ff867a28db569e000c4a
+#I lost internet connection in last 15 minutes while these
+#2 were being analyzed will finish it after
+#recording of Day 33 gets uploaded
