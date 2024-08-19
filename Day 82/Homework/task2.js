09ce71e118112f48b1a722e6ef8fce6eb2d0a013
RoundLuka <lukagurgenidze282007@gmail.com>
2024-08-19 23:29:45 +0400

    commit

---
+++ Day 82/Homework/task2.js
@@ -0,0 +1,63 @@
+// 2) გააკეთეთ 10-10 მაგალითი  while, for and if-else ზე
+
+
+// task1 while: 
+// Count Down: Use a while loop to print numbers from 10 down to 1.
+
+// let i = 10;
+// while(i > 0){
+//     console.log(i);
+//     i --;
+// }
+
+// task2 while:
+// Sum Until Negative:Continuously ask the user for numbers and add 
+// them together until a negative number is entered. Print the sum.
+
+// let sum = 0;
+// let num;
+
+// while(num >= 0){
+//     sum += num;
+// }
+
+// console.log(sum)
+
+// task3 while:
+// Factorial Calculation: Calculate the factorial of a given number using a while loop.
+
+// let number = Number(prompt("Please enter a number"))
+
+// let product = 1;
+// let i = 1;
+
+// while(i !== number){
+//     product = product * i
+//     i ++
+// }
+
+// console.log(product)
+
+
+
+
+
+
+
+// bonus
+
+// const pin = "1234"
+// let count = 3;
+// let code;
+
+// while(code !== pin && count > 0){
+//     code = prompt("Please enter pin code");
+//     if(code == pin){
+//         alert("Acess granted!");
+//     }
+//     else{
+//         count--;
+//         alert("Invalid pin code, attempts left " + attempts);
+//     }
+
+// }
