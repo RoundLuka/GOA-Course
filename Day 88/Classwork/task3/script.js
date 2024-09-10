4f91f22045db3b35808176c28a0409d616f65789
RoundLuka <lukagurgenidze282007@gmail.com>
2024-09-10 16:27:57 +0400

    commit

--- Day 88/Classwork/task3/script.js
+++ Day 88/Classwork/task3/script.js
@@ -4,18 +4,34 @@
 // საბოლოოდ შეადარეთ კომპიუტერისა და მომხმარებლის არჩევანი და 
 // დაითვალეთ ქულები, ფრე, მოგება და წაგება
 
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scBtn = document.getElementById("sc");
+const myScoreP = document.getElementById('my-score');
+const compScoreP = document.getElementById('comp-score');
+const winsP = document.getElementById('wins');
+const btnsDiv = document.getElementById('btns');
 
-function moveGenerator(){
-    const optionArr = ["rock","paper","sc"];
-    let choice = Math.floor(Math.random(0,2))
-    console.log(choice)
-}
+const choices = ['rock', 'paper','scissors'];
 
-function scoreCount(){
+let myScore = 0;
+let compScore = 0;
 
-}
+btnsDiv.addEventListener('click', function(e) {
+    const btnId = e.target.id;
 
-rockBtn.addEventListener("click", moveGenerator)
+    const compChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    if(btnId === compChoice) {
+        winsP.textContent = 'Its a tie!';
+    } 
+    
+    else if((btnId === "rock" && compChoice === "paper") || (btnId === "scissors" && compChoice === "rock") || (btnId === "paper" && compChoice === "scissors")) {
+        compScore++;
+        compScoreP.textContent = compScore;
+        winsP.textContent = 'You lost!';
+    } 
+    
+    else {
+        myScore++;
+        myScoreP.textContent = myScore;
+        winsP.textContent = 'You won!';
+    }
+});
