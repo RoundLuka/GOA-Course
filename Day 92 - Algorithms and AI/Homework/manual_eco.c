06bc6fb6e9b94293e97faf033de53ca04c27cadd
RoundLuka <lukagurgenidze282007@gmail.com>
2024-09-25 14:25:38 +0400

    commit

---
+++ Day 92 - Algorithms and AI/Homework/manual_eco.c
@@ -0,0 +1,48 @@
+// ექოს კოდი გადაწერეთ ისე რომ განასხვავოთ 2 შეცდომა EOF-ის დაბრუნების შემთხვევაში, 
+// გაარკვიეთ როგორ მიიღეთ EOF ერორების მიხედვით feof, ferror, დაწერეთ ფუნქციები
+// my_getchar და my_putchar. გამოიყენეთ if
+
+#include <stdio.h>
+
+
+int my_getchar()
+{
+    int c;
+    
+    if((c = getchar()) == EOF)
+    {
+        if (ferror(stdin))
+        {
+            perror("error indicator is set");
+            exit(1)
+        }
+        if(feof(stdin))
+        {
+            perror("end-of-file indicator is set");
+            exit(3)
+        }
+    perror("getchar returned EOF when eror indicator and end-of-file indicator ARE NOT SET");
+    exit(2)
+    }
+    
+    return c;
+}
+
+
+int my_putchar(int c)
+{
+    if (putchar(c) == EOF) 
+    {
+        if (ferror(stdout)) 
+        {
+            perror("error indicator is set for output");
+            exit(1); 
+        }
+        else
+        {
+            perror("putchar returned EOF without an error indicator");
+            exit(2); 
+        }
+    }
+    return c; 
+}
--- Day 94/Homework/script.js
+++ Day 94/Homework/script.js
@@ -1,8 +1,8 @@
 // 1. **Filter Prime Numbers**
 //    - Create an array of numbers and use `filter()` to return only the prime numbers.
 
-const numArr = [1,2,3,4,5,6,7,8];
+// const numArr = [1,2,3,4,5,6,7,8];
 
-const onlyPrime = numArr.filter((num) => {
-    for(let i = 0; )
-});
+// const onlyPrime = numArr.filter((num) => {
+//     for(let i = 0; )
+// });
