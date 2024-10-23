bcf6b04d3b3eca5ab90e97c6d353064cf032cdaf
RoundLuka <lukagurgenidze282007@gmail.com>
2024-10-23 23:58:18 +0400

    commit

--- Day 097/Classwork/task3/script.js
+++ Day 097/Classwork/task3/script.js
@@ -4,8 +4,24 @@ class Account {
     constructor(firstName, lastName, email, password, city){
         this.FirstName = firstName;
         this.LastName = lastName;
-        
+        this.Email = email;
+        this.Password = password;
+        this.City = city;
     }
+    static printDetails(password){
+        for(let i = 0; i < accounts.length; i++){
+            if(password === accounts[i].Password){
+                const currentUser = accounts[i];
+                console.log(`First Name: ${currentUser.FirstName}`);
+                console.log(`Last Name: ${currentUser.LastName}`);
+                console.log(`Email: ${currentUser.Email}`);
+                console.log(`Password: ${currentUser.Password}`);
+                console.log(`City: ${currentUser.City}`);
+            } else {
+                console.log("Account not found");
+            };
+        };
+    };
 }
 
 let accounts = [];
@@ -19,5 +35,11 @@ form.addEventListener("submit", (e) => {
     const password = form.Password.value;
     const city = form.City.value;
 
-    const account = {}
+    const account = new Account(firstName, lastName, email, password, city);
+
+    accounts.push(account);
 });
+
+setInterval(() => {
+    Account.printDetails("luka1234");
+}, 50000);
