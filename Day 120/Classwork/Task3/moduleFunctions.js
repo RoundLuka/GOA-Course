df4bce444de914a28fd2c8faf1f2bb26b96a8e95
RoundLuka <lukagurgenidze282007@gmail.com>
2024-11-09 23:45:00 +0400

    commit

---
+++ Day 120/Classwork/Task3/moduleFunctions.js
@@ -0,0 +1,30 @@
+const calculator = (num1, num2, operator) => {
+    if(operator === "+") {
+        console.log(num1 + num2);
+    } else if (operator === "-") {
+        console.log(num1 - num2)
+    } else if (operator === "*") {
+        console.log(num1 * num2)
+    } else if (operator === "/") {
+        console.log(num1 / num2);
+    } else {
+        console.log("Invalid operation");
+    }
+};
+
+const filter = (arr, callback) => {
+    let resArr = [];
+
+    for(const value of arr){
+        if(callback(value)) {
+            resArr.push(value);
+        }
+    }
+    return resArr;
+}
+
+export {calculator, filter};
+
+// Default export, exporting function by its name "filter", when importing can change its name.
+// From each file can only use 1 default export
+// export default filter
